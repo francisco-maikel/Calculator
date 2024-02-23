b83d0d03a6b21228e9f616f04f7321b69ffec25d
Franisco maikel <francisco.gomes664@aluno.ce.gov.br>
2024-02-23 16:15:29 -0300

    add project script

---
+++ scripts/scripts.js
@@ -0,0 +1,47 @@
+const expression = document.querySelector(".expression");
+const expressionDisplay = document.querySelector(".expressionDisplay");
+const buttons = document.querySelectorAll(".gridButtons button");
+
+function clreanScreen() {
+  expression.innerHTML = "";
+  expressionDisplay.innerHTML = "";
+}
+
+const totalNumbers = 13;
+let isError = false;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (isError) {
+        clreanScreen();
+        isError = false;
+    }
+
+    switch (button.innerHTML) {
+      case "C":
+        clreanScreen();
+        break;
+      case "DEL":
+        expression.innerHTML = expression.innerHTML.slice(0, -1);
+        break;
+      case "=":
+        try {
+            if (expression.innerHTML.length > 0) {
+              expressionDisplay.innerHTML = expression.innerHTML
+expression.innerHTML = String (
+            expression.innerHTML = 
+eval(expression.innerHTML.replace("x", "*").replace("%", "/100")
+  )).slice(0, 13);
+            }     
+        } catch (error) {
+          console.error(error);
+          expression.innerHTML = "Error";
+        }
+        break;
+      default:
+        if (expression.innerHTML.length >= 13) return
+        expression.innerHTML += button.innerHTML;
+        break;
+    }
+  });
+});
