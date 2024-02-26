74707fb7e6639f7d6b11c1e65969bb6ec8e93018
Franisco maikel <francisco.gomes664@aluno.ce.gov.br>
2024-02-26 16:18:40 -0300

    add modal history

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,17 @@
 const expression = document.querySelector(".expression");
 const expressionDisplay = document.querySelector(".expressionDisplay");
 const buttons = document.querySelectorAll(".gridButtons button");
+const buttonHistory = document.querySelector(".buttonHistory");
+const modalHistory = document.querySelector(".modalHistory");
+const buttonCloseModal = document.querySelector(".buttonCloseModal");
+
+buttonHistory.addEventListener("click", () => {
+  modalHistory.showModal();
+});
+
+buttonCloseModal.addEventListener("click", () => {
+  modalHistory.close();
+});
 
 function clreanScreen() {
   expression.innerHTML = "";
@@ -13,8 +24,8 @@ let isError = false;
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (isError) {
-        clreanScreen();
-        isError = false;
+      clreanScreen();
+      isError = false;
     }
 
     switch (button.innerHTML) {
@@ -26,20 +37,21 @@ buttons.forEach((button) => {
         break;
       case "=":
         try {
-            if (expression.innerHTML.length > 0) {
-              expressionDisplay.innerHTML = expression.innerHTML
-expression.innerHTML = String (
-            expression.innerHTML = 
-eval(expression.innerHTML.replace("x", "*").replace("%", "/100")
-  )).slice(0, 13);
-            }     
+          if (expression.innerHTML.length > 0) {
+            expressionDisplay.innerHTML = expression.innerHTML;
+            expression.innerHTML = String(
+              (expression.innerHTML = eval(
+                expression.innerHTML.replace("x", "*").replace("%", "/100")
+              ))
+            ).slice(0, 13);
+          }
         } catch (error) {
           console.error(error);
           expression.innerHTML = "Error";
         }
         break;
       default:
-        if (expression.innerHTML.length >= 13) return
+        if (expression.innerHTML.length >= 13) return;
         expression.innerHTML += button.innerHTML;
         break;
     }
